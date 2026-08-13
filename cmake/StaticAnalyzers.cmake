88be44520894cc5f8fb9db4073d4cd0cd8cd48b7
Jan Aleksander Górski <jan.a.gorski@wp.pl>
2026-08-13 23:17:17 +0900

    fix: disable clang-tidy when not using a Clang compiler

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -55,50 +55,37 @@ macro(myproject_enable_clang_tidy target WARNINGS_AS_ERRORS)
 
   find_program(CLANGTIDY clang-tidy)
   if(CLANGTIDY)
-    if(NOT
-       CMAKE_CXX_COMPILER_ID
-       MATCHES
-       ".*Clang")
-
-      get_target_property(TARGET_PCH ${target} INTERFACE_PRECOMPILE_HEADERS)
-
-      if("${TARGET_PCH}" STREQUAL "TARGET_PCH-NOTFOUND")
-        get_target_property(TARGET_PCH ${target} PRECOMPILE_HEADERS)
+    if(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+      message(
+        STATUS
+          "clang-tidy disabled: ${CMAKE_CXX_COMPILER_ID} libstdc++ headers are not reliably parsed by clang-tidy")
+    else()
+      set(CLANG_TIDY_OPTIONS
+          ${CLANGTIDY}
+          -extra-arg=-Wno-unknown-warning-option
+          -extra-arg=-Wno-ignored-optimization-argument
+          -extra-arg=-Wno-unused-command-line-argument
+          -p)
+      # set standard
+      if(NOT
+         "${CMAKE_CXX_STANDARD}"
+         STREQUAL
+         "")
+        if("${CLANG_TIDY_OPTIONS_DRIVER_MODE}" STREQUAL "cl")
+          set(CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=/std:c++${CMAKE_CXX_STANDARD})
+        else()
+          set(CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=-std=c++${CMAKE_CXX_STANDARD})
+        endif()
       endif()
 
-      if(NOT ("${TARGET_PCH}" STREQUAL "TARGET_PCH-NOTFOUND"))
-        message(
-          SEND_ERROR
-            "clang-tidy cannot be enabled with non-clang compiler and PCH, clang-tidy fails to handle gcc's PCH file")
+      # set warnings as errors
+      if(${WARNINGS_AS_ERRORS})
+        list(APPEND CLANG_TIDY_OPTIONS -warnings-as-errors=*)
       endif()
-    endif()
 
-    # construct the clang-tidy command line
-    set(CLANG_TIDY_OPTIONS
-        ${CLANGTIDY}
-        -extra-arg=-Wno-unknown-warning-option
-        -extra-arg=-Wno-ignored-optimization-argument
-        -extra-arg=-Wno-unused-command-line-argument
-        -p)
-    # set standard
-    if(NOT
-       "${CMAKE_CXX_STANDARD}"
-       STREQUAL
-       "")
-      if("${CLANG_TIDY_OPTIONS_DRIVER_MODE}" STREQUAL "cl")
-        set(CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=/std:c++${CMAKE_CXX_STANDARD})
-      else()
-        set(CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=-std=c++${CMAKE_CXX_STANDARD})
-      endif()
+      message("Also setting clang-tidy globally")
+      set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_OPTIONS})
     endif()
-
-    # set warnings as errors
-    if(${WARNINGS_AS_ERRORS})
-      list(APPEND CLANG_TIDY_OPTIONS -warnings-as-errors=*)
-    endif()
-
-    message("Also setting clang-tidy globally")
-    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_OPTIONS})
   else()
     message(WARNING "clang-tidy requested but executable not found")
   endif()
